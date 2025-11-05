1d1795509918ed29a6310e60980ba7eae001b9b7
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:46:16 +0700

    Golang conversion basic

---
+++ conversion_2.go
@@ -0,0 +1,13 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	var name = "Yanuar Christy"
+// 	var e uint8 = name[0]
+// 	var eString = string(e)
+
+// 	fmt.Println(name)
+// 	fmt.Println(e)
+// 	fmt.Println(eString)
+// }
