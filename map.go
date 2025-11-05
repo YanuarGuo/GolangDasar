22dc0af91eb6f1a4a5e403d9788fe7543b486ec3
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:47:24 +0700

    Golang map basic

---
+++ map.go
@@ -0,0 +1,13 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	person := map[string]string{
+// 		"name":    "Yanuar",
+// 		"address": "Salatiga",
+// 	}
+
+// 	fmt.Println(person["name"])
+// 	fmt.Println(person["address"])
+// }
---
+++ map_2.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	book := map[string]string{
+// 		"title":  "Judul",
+// 		"author": "Yanuar",
+// 		"ups":    "Salah",
+// 	}
+// 	fmt.Println(book)
+
+// 	delete(book, "ups")
+// 	fmt.Println(book)
+// }
