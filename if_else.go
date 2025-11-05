2683243cc27b8fccad53796bdca1bf6569cae65c
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:48:45 +0700

    Golang if else basic

---
+++ if_else.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	name := "Yanuar"
+
+// 	if name == "Ade" {
+// 		fmt.Println("Halo" + name)
+// 	} else if name == "Yanuar" {
+// 		fmt.Println("Halo " + name)
+// 	} else {
+// 		fmt.Println("Maaf, halo " + name)
+// 	}
+
+// }
---
+++ if_else_2.go
@@ -0,0 +1,13 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	name := "Yanuar"
+
+// 	if length := len(name); length > 5 {
+// 		fmt.Println("Terlalu panjang")
+// 	} else {
+// 		fmt.Println("Pas")
+// 	}
+// }
