d7bd85d0ec560bcc9676ddeb749aeb7c504e92b5
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:45:16 +0700

    Golang array vs slice

---
+++ array_vs_slice.go
@@ -0,0 +1,11 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	array := [...]string{"Array", "Array"}
+// 	slice := []string{"Slice", "Slice"}
+
+// 	fmt.Println(array)
+// 	fmt.Println(slice)
+// }
