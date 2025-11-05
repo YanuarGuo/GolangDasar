95c997699906905b4e92448e3e50016389cef54f
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:43:48 +0700

    Golang compare basic

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// const firstName = "Yanuar"
+// const lastName = "Utama"
+
+// fmt.Println(firstName)
+// fmt.Println(lastName)
+
+// Nilai const tidak bisa diubah lagi!
+// firstName = "Christy"
+// lastName = "Ade"
+// }
---
+++ constant_2.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	const (
+// 		firstName = "Yanuar"
+// 		lastName  = "Utama"
+// 	)
+
+// 	fmt.Println(firstName)
+// 	fmt.Println(lastName)
+
+// }
