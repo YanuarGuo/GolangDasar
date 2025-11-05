23d6778b71d35bed135cf08d377e3010609999bc
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:43:33 +0700

    Golang compare basic

---
+++ compare.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main(){
+// 	var compare1 = "Iya"
+// 	var compare2 = "Iya"
+// 	var compare3 = "Oke"
+
+// 	var result1 bool = compare1 == compare2
+// 	var result2 bool = compare1 == compare3
+
+// 	fmt.Println(result1)
+// 	fmt.Println(result2)
+// }
---
+++ compare_2.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main(){
+// 	var compare1 = 1
+// 	var compare2 = 2
+// 	var compare3 = 3
+
+// 	var result1 bool = compare1 < compare2
+// 	var result2 bool = compare2 > compare3
+
+// 	fmt.Println(result1)
+// 	fmt.Println(result2)
+// }
