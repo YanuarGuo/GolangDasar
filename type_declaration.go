fefa0180898e6335a8281504a32772c3446d5dc7
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-05 12:44:57 +0700

    Golang type declaration

---
+++ type_declaration.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main(){
+// 	type NoKTP string
+
+// 	var ktpYanuar NoKTP = "0102030405060708"
+// 	var contoh string = "1122334455667788"
+// 	var contohKtp NoKTP = NoKTP(contoh)
+
+// 	fmt.Println(ktpYanuar)
+// 	fmt.Println(contoh)
+// 	fmt.Println(contohKtp)
+// }
