8c1073d94ba9a23e117b6ab528d9cc96e8dd95af
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 09:54:16 +0700

    Golang interface basic

---
+++ interface.go
@@ -0,0 +1,39 @@
+package main
+
+// import "fmt"
+
+// type HasName interface {
+// 	GetName() string
+// }
+
+// type Person struct {
+// 	Name string
+// }
+
+// type Animal struct {
+// 	Name string
+// }
+
+// func SayHello(value HasName) {
+// 	fmt.Println("Hi", value.GetName())
+// }
+
+// func (person Person) GetName() string {
+// 	return person.Name
+// }
+
+// func (animal Animal) GetName() string {
+// 	return animal.Name
+// }
+
+// func main() {
+// 	person := Person{
+// 		Name: "Yanuar",
+// 	}
+// 	SayHello(person)
+
+// 	animal := Animal{
+// 		Name: "Gajah",
+// 	}
+// 	SayHello(animal)
+// }
