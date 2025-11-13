773db7cef3af263cdae9123b633953fb948ca286
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 09:55:36 +0700

    Golang factorial recursive basic

---
+++ function_factorial_recursive.go
@@ -0,0 +1,17 @@
+package main
+
+// import "fmt"
+
+// func newFunction(value int) int {
+// 	if value == 1 {
+// 		return 1
+// 	} else {
+// 		return value * newFunction(value-1)
+// 	}
+// }
+
+// func main() {
+// 	result := 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+// 	fmt.Println(result)
+// 	fmt.Println(newFunction(10))
+// }
