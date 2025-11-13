05635e51a495619059b3b5a4466a8d9f17373df5
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 09:53:44 +0700

    Golang defer basic

---
+++ defer.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+// func logging() {
+// 	fmt.Println("Defer adalah fitur untuk pemanggilan function setelah function selesai")
+// }
+
+// func runApp() {
+// 	defer logging()
+// 	fmt.Println("Panggil Run App")
+// }
+
+// func main() {
+// 	runApp()
+// }
