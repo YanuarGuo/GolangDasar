cacaed1e287d5a74f9f1b9b7d860d3f337630c53
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 09:55:13 +0700

    Golang empty function (any) basic

---
+++ any.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// func Kosong() any {
+// 	// return 1
+// 	// return true
+// 	return "any"
+// }
+
+// func main() {
+// 	var kosong any = Kosong()
+// 	fmt.Println(kosong)
+// }
