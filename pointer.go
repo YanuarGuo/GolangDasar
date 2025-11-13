84c85dd7e6c65a609ca7773dc28e9ddbde395af6
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 10:32:17 +0700

    Golang basic pointer

---
+++ pointer.go
@@ -0,0 +1,27 @@
+package main
+
+// import "fmt"
+
+// type Address struct {
+// 	City, Province, Country string
+// }
+
+// func main() {
+// pass by value
+// address1 := Address{"Salatiga", "Jawa Tengah", "Indonesia"}
+// address2 := address1 // address 2 merupakan copy dari address1 bukan address1 sendiri
+
+// address2.City = "Semarang"
+
+// fmt.Println(address1)
+// fmt.Println(address2)
+
+// pass by reference
+// address1 := Address{"Salatiga", "Jawa Tengah", "Indonesia"}
+// address2 := &address1 // address 2 merupakan copy dari address1 bukan address1 sendiri
+
+// address2.City = "Semarang"
+
+// fmt.Println(address1)
+// fmt.Println(address2)
+// }
