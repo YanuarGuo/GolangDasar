b879e16fd0cc24b5ec5e3512d413b5a0a1969525
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 09:53:30 +0700

    Golang panic basic

---
+++ panic.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+// func endApp() {
+// 	fmt.Println("Panic adalah fitur untuk menghentikan program")
+// }
+
+// func runApp(err bool) {
+// 	defer endApp()
+// 	if err {
+// 		panic("Error")
+// 	}
+// 	fmt.Println("Panggil Run App")
+// }
+
+// func main() {
+// 	runApp(true)
+// }
