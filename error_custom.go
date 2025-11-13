d8705f1a46dd371de47328b529dd4f96f3787011
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 13:13:59 +0700

    Golang error and error customized

---
+++ error_custom.go
@@ -0,0 +1,49 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// type validationError struct {
+// 	Message string
+// }
+
+// type notFoundError struct {
+// 	Message string
+// }
+
+// func (v *validationError) Error() string {
+// 	return v.Message
+// }
+
+// func (n *notFoundError) Error() string {
+// 	return n.Message
+// }
+
+// func SaveData(id string, data any) error {
+// 	if id == "" {
+// 		return &validationError{"Validation error"}
+// 	}
+
+// 	if id == "Yanuar" {
+// 		return &notFoundError{"Data not found"}
+// 	}
+
+// 	return nil
+// }
+
+// func main() {
+// 	err := SaveData("Joko", nil)
+
+// 	if err != nil {
+// 		if validationErr, ok := err.(*validationError); ok {
+// 			fmt.Println("Validation error:", validationErr.Error())
+// 		} else if notFoundError, ok := err.(*notFoundError); ok {
+// 			fmt.Println("Not found error:", notFoundError.Error())
+// 		} else {
+// 			fmt.Println("Unknown error:", err.Error())
+// 		}
+// 	} else {
+// 		fmt.Println("Sukses")
+// 	}
+// }
