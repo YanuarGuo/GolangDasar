26a43ad818a4ada5bde56a7810115d071c8ba2d8
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 09:53:14 +0700

    Golang recover basic

---
+++ recover.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+// func endApp() {
+// 	message := recover()
+// 	fmt.Println("Recover adalah fitur untuk menangkap data panic", message)
+// }
+
+// func runApp(err bool) {
+// 	defer endApp()
+// 	if err {
+// 		panic("Error")
+// 	}
+// }
+
+// func main() {
+// 	runApp(true)
+// }
