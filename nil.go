a7e0140dd35d1aa4121fd0e65719fcf29067289a
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 10:03:05 +0700

    Golang nil basic

---
+++ nil.go
@@ -0,0 +1,39 @@
+package main
+
+// import "fmt"
+
+/*
+
+Contoh tidak bisa
+Karena nil hanya bisa digunakan pada interface, map, slice, pointer, dan channel
+
+*/
+
+// func Contoh(name string) string {
+// 	if name == "" {
+// 		return nil
+// 	} else {
+// 		return name
+// 	}
+// }
+
+// func NewMap(name string) map[string]string {
+// 	if name == "" {
+// 		return nil
+// 	} else {
+// 		return map[string]string{
+// 			"name": name,
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	data := NewMap("Yanuar")
+
+// 	if data == nil {
+// 		fmt.Println("Data kosong")
+// 	} else {
+// 		fmt.Println(data["name"])
+// 	}
+
+// }
