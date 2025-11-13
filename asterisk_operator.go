03924a23b3747c2764b874363da53191fc610c34
YanuarGuo <yanuar.utama14@gmail.com>
2025-11-13 10:53:19 +0700

    Golang asterisk operator basic

---
+++ asterisk_operator.go
@@ -0,0 +1,23 @@
+package main
+
+// import "fmt"
+
+// type Address struct {
+// 	City, Province, Country string
+// }
+
+// func main() {
+
+// 	address1 := Address{"Salatiga", "Jawa Tengah", "Indonesia"}
+// 	address2 := &address1 // address 2 merupakan copy dari address1 bukan address1 sendiri
+
+// 	address2.City = "Semarang"
+
+// 	fmt.Println(address1)
+// 	fmt.Println(address2)
+
+// 	// address2 = &Address{"Jakarta", "DKI Jakarta", "Indonesia"} // pointer
+// 	*address2 = Address{"Jakarta", "DKI Jakarta", "Indonesia"} // asterisk
+// 	fmt.Println(address1)
+// 	fmt.Println(address2)
+// }
